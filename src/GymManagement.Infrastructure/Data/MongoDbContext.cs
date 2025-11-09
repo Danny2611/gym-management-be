ef379ae28df4d6515faca1ed1402fd3ea6ba7599
QuocTrung26 <Trung.le@2025>
2025-11-09 15:24:23 +0700

    config connect datase

---
+++ src/GymManagement.Infrastructure/Data/MongoDbContext.cs
@@ -0,0 +1,26 @@
+using GymManagement.Domain.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace GymManagement.Infrastructure.Data
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(IConfiguration configuration)
+        {
+            var connectionString = configuration["Database:Mongo:ConnectionString"];
+            var databaseName = configuration["Database:Mongo:DatabaseName"];
+
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase(databaseName);
+        }
+
+        public IMongoCollection<Member> Members => _database.GetCollection<Member>("members");
+        
+        // Thêm các collection khác khi cần
+        // public IMongoCollection<Package> Packages => _database.GetCollection<Package>("packages");
+        // public IMongoCollection<Trainer> Trainers => _database.GetCollection<Trainer>("trainers");
+    }
+}
